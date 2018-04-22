02389b29691a8efd8c4396e1a23bbaef27b30c40
sam-by-name <sam-sanders@hotmail.co.uk>
2018-04-22 21:17:12 +1200

    backgrounds and difficulties

--- minesweeper.js
+++ minesweeper.js
@@ -46,55 +46,56 @@ document.addEventListener('DOMContentLoaded', startGame)
     { row: 5, col: 4, isMine: "", isMarked: "", hidden: true },
     { row: 5, col: 5, isMine: "", isMarked: "", hidden: true },
   ]}*/
-var board;
+  //var userChoice = prompt("How easy do you want Life to be? 2-12");
+  var board;
+  var grid = 6;
 
 function initGrid () {
   board = {
     cells:[]
   }
-  var grid = 6;
   for (var x = 0; x < grid; x++) {
     for (var y = 0; y < grid; y ++) {
       board.cells.push ({
         row: x,
         col: y, 
-        isMine: "", 
+        isMine: Math.floor(Math.random()*1.3), 
         isMarked: false, 
         hidden: true 
       })
     }
   }
 }
-
+/* I used this until i found the easier way 
 function initBooms ()  {
   for (var i = 0; i < Math.floor(board.cells.length / 4); i++)  {
     board.cells[Math.floor(Math.random() * board.cells.length)].isMine = true;
   }
-}
+}*/
 
 
 function startGame () {
   initGrid ()
-  initBooms()
   for (let i = 0; i < board.cells.length; i++) {
   board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);  
   } 
   lib.initBoard();
-  document.addEventListener('click', checkForWin);
+  document.addEventListener('click', theEnd, checkForWin);
   document.addEventListener('contextmenu', checkForWin);
 }
-
 function checkForWin () {
   for (let i = 0; i < board.cells.length; i++) {
     let check = board.cells[i];
     if (check.isMine && !check.isMarked){
       return;
-    }
-    else if (!check.isMine && check.hidden) {
+    } else if (!check.isMine && check.hidden) {
       return;
     } 
   }
   lib.displayMessage('You win!');
+  document.getElementById('victory').play();
+  //var audio = new Audio('victory.mp3');
+  //audio.play();
 }
 function countSurroundingMines (cell) {
 
@@ -111,9 +112,45 @@ function countSurroundingMines (cell) {
 function retry () {
   removeListeners ();
   restart ();
-  startGame (); // after this point it does not work and tells me that 36 cells is the max ... ?
+  startGame (); 
+}
+function retryEasy () {
+  removeListeners ();
+  restart ();
+  grid = 3;
+  document.getElementById('body').style.background = 'url("cloud.jpeg") 100%'; 
+  startGame ();
+}
+function retryMed () {
+  removeListeners ();
+  restart ();
+  grid = 6;
+  document.getElementById('body').style.background = 'url(nightsky.jpg)'; 
+  startGame (); 
+}
+function retryHard () {
+  removeListeners ();
+  restart ();
+  grid = 9;
+  document.getElementById('body').style.background = 'url(nightsky.jpg)'; 
+  startGame (); 
+}
+function retryVeryHard () {
+  removeListeners ();
+  restart ();
+  grid = 12;
+  document.getElementById('body').style.background = 'url(nightsky.jpg)'; 
+  startGame (); 
 }
 function restart () {
   let board = document.getElementsByClassName('board')[0];
   board.innerHTML = '';
+}
+function theEnd () {
+  for (let i = 0; i < board.cells.length; i++) {
+    let check = board.cells[i];
+    if (check.isMine && !check.hidden) {
+      document.getElementById('gameOver').play();
+    }
+  }
 }
